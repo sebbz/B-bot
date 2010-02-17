b2ac49377fa1bac6dd2ad937f164139af7bd5078
Sebastian Jansson <sebbz@sebbz.se>
2010-02-17 18:57:08 +0100

    Initial commit

---
+++ src/stats.py
@@ -0,0 +1,18 @@
+'''
+@author: han som inte får commita
+'''
+
+class Stats(object):
+    """
+    Lögn, förbannad lögn och statistik. 100.000 fler sysselsatta idag, sade statsminister Fredrik Reinfeldt i partiledardebatten i Agenda igår.
+    """
+
+    def __init__(self):
+        pass 
+
+class Database():
+    def __init__(self, dbfile):
+        self.dbfile = dbfile
+    
+    def createTables(self):
+        pass
