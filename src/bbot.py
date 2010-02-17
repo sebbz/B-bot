75864867373e839bae3ee989fcd34b3e247967f6
grul <grul@grul.nu>
2010-02-17 20:54:22 +0100

    glöm inte lägga in det där grejen sebbz sa

---
+++ src/bbot.py
@@ -0,0 +1,70 @@
+# coding=utf-8
+import sys
+import socket
+import signal
+
+import gamemaster
+
+def signal_handler(signal, frame):
+    global b
+    del(b)
+
+class BBot:
+    def __del__(self):
+        self.s.close()
+        print "TERMINATING"
+
+    def __init__(self):
+        self.buffer = ""
+        self.HOST="irc.se.quakenet.org"
+        self.PORT=6667
+        self.NICK="TALKSHOWHOST"
+        self.IDENT="b-boten"
+        self.REALNAME="b botson"
+        self.OWNER="grul"
+        self.CHANNEL="#b-game"
+
+        self.s=socket.socket()
+        print "Connecting..."
+        self.s.connect((self.HOST, self.PORT))
+        print "Sending NICK..."
+        self.s.send("NICK %s\r\n" % self.NICK)
+        print "Sending USER..."
+        self.s.send("USER %s %s %s :%s\r\n" % (self.IDENT, self.IDENT, self.IDENT, self.REALNAME))
+
+        self.pings = 0
+
+        self.gm = gamemaster.GameMaster(self)
+
+        print "Main loop..."
+        while True:
+            self.buffer += self.s.recv(2048)
+            lines = self.buffer.split("\n")
+            self.buffer = lines.pop()
+            for line in lines:
+                print line
+                if line.find("PING") != -1:
+                    self.pings += 1
+                    self.s.send("PONG %s\r\n" % line.split()[1])
+                    if self.pings == 2:
+                        self.s.send("JOIN %s\r\n" % self.CHANNEL)
+                if line.find("PRIVMSG") != -1:
+                    print line
+                    nick = line.split()[0].split(":")[1].split("!")[0]
+                    print "nick: %s" % nick
+                    message = line.split(" ", 3)[3][1:]
+                    print "message: %s" % message
+
+                    if message.startswith("b."):
+                        l = self.gm.handleMessage(nick, message[2:])
+                        if l != "":
+                            self.s.send("PRIVMSG %s :%s\r\n" % (self.CHANNEL, l))
+    def sendPrivMsg(self, nick, msg):
+        self.s.send("PRIVMSG %s :%s\r\n" % (nick, msg))
+
+    def sendChanMsg(self, msg):
+        self.s.send("PRIVMSG %s :%s\r\n" %(self.CHANNEL, msg))
+
+if __name__ == "__main__":
+    b = BBot()
+    signal.signal(signal.SIGINT, signal_handler)
--- src/gamemaster.py
+++ src/gamemaster.py
@@ -1,8 +1,48 @@
+# coding=utf-8
+import time
 import b
+import bbot
 
-class GameMaster():
-    def __init__(self):
-        pass
-    def handlemessage(self, msg):
-        return msg
+class GameMaster:
+    def __init__(self, bbot):
+        self.playing = False
+        self.bbot = bbot
+    def handleMessage(self, nick, msg):
+        msg = msg.strip()
+        if msg == "new game":
+            return self.newGame(nick)
+        if msg == "start game":
+            return self.startGame(nick)
+        if msg == "help":
+            pass
+        if msg == "draw":
+            pass
+        if msg == "join":
+            return self.join(nick)
+        return "fuck OFF %s!!!!!!!" % nick.upper()
 
+    def newGame(self, nick):
+        if self.playing:
+            return "A game is already in progress..."
+        self.b = b.B(nick)
+        self.b.addPlayer(nick)
+        self.playing = True
+        return "A new game has been created by %s. Type b.join to join in" % nick
+
+    def join(self, nick):
+        str = "Couldn't add %s. Players: %s " % (nick, " ".join(self.b.getPlayers()))
+        if self.b.addPlayer(nick):
+            str = "%s joined! Players: " % nick
+            str += " ".join(self.b.getPlayers())
+        return str
+
+    def startGame(self, nick):
+        if not self.playing:
+            return "No game to start"
+        if self.b.getStartPlayer() != nick:
+            return "Only %s can start the game" % self.b.getStartPlayer()
+        if len(self.b.getPlayers()) < 2:
+            return "You need more players!!!"
+        self.bbot.sendChanMsg("Deck has been sorted")
+        self.bbot.sendPrivMsg("grul", "HEJ!!!!!!!!!")
+        self.b.shuffleDeck()
