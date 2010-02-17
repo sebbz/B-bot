fe37a5313fbe77ff31c72831ddd68bc8d6e24b54
grul <grul@grul.nu>
2010-02-17 16:43:18 +0100

    yep, now it connects and joins #b-game on q-net

--- src/b-bot.py
+++ src/b-bot.py
@@ -4,35 +4,52 @@ import socket
 import string
 import signal
 
-import b
+#import b
 
 def signal_handler(signal, frame):
     global s
-    print "lol"
+    print """
+############
+TERMINATING
+############"""
     s.close()
     sys.exit()
 
-
 signal.signal(signal.SIGINT, signal_handler)
+
+buffer = ""
+
 HOST="irc.se.quakenet.org"
 PORT=6667
 NICK="stolpen_kok"
 IDENT="b-boten"
 REALNAME="b botson"
 OWNER="grul"
-CHANNELINIT="#it05 apknull"
-readbuffer=""
+CHANNEL="#b-game"
 
 s=socket.socket()
+print "Connecting..."
 s.connect((HOST, PORT))
-s.send("NICK "+NICK+"\r\n")
-s.send("USER "+IDENT+" "+HOST+" :"+REALNAME+"\r\n")
+print "Sending NICK..."
+s.send("NICK %s\r\n" % NICK)
+print "Sending USER..."
+s.send("USER %s %s %s :%s\r\n" % (IDENT, IDENT, IDENT, REALNAME))
 
-done = False
-while not done:
-    line = s.recv(500)
-    print line
+joined = False
 
+#game = b.B()
+print "Main loop..."
+while True:
+    buffer += s.recv(2048)
+    lines = buffer.split("\n")
+    buffer = lines.pop()
+    for line in lines:
+        print line
+        if line.find("PING") != -1:
+            s.send("PONG %s\r\n" % line.split()[1])
+            if not joined:
+                s.send("JOIN %s\r\n" % CHANNEL)
+                joined = True
 
 s.close()
 
