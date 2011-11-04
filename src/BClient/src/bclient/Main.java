11e3db4f4be055347881430758a965f3fc131d99
Sebastian Jansson <sebbz@sebbz.se>
2011-11-04 22:40:59 +0100

    Huvudfönster + två kort + meny för att avsluta

--- src/BClient/src/bclient/Main.java
+++ src/BClient/src/bclient/Main.java
@@ -16,6 +16,8 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        MainWindow a = new MainWindow();
+        a.setVisible(true);
     }
 
 }
