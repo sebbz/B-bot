1dbb681ebf8c8e1cd031c27476bab2e789b6cbcc
Sebastian Jansson <sebbz@sebbz.se>
2011-11-04 21:30:12 +0100

    Labels istället för annat

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'main.ui'
 #
-# Created: Fri Nov  4 20:58:59 2011
+# Created: Fri Nov  4 21:30:06 2011
 #      by: PyQt4 UI code generator 4.8.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -23,15 +23,17 @@ class Ui_MainWindow(object):
         self.label = QtGui.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(30, 20, 171, 21))
         self.label.setObjectName(_fromUtf8("label"))
-        self.firstCard = QtGui.QGraphicsView(self.centralwidget)
-        self.firstCard.setGeometry(QtCore.QRect(30, 100, 161, 241))
-        self.firstCard.setObjectName(_fromUtf8("firstCard"))
-        self.secondCard = QtGui.QGraphicsView(self.centralwidget)
-        self.secondCard.setGeometry(QtCore.QRect(200, 100, 161, 241))
-        self.secondCard.setObjectName(_fromUtf8("secondCard"))
         self.pushButton = QtGui.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(290, 40, 93, 27))
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.firstCard = QtGui.QLabel(self.centralwidget)
+        self.firstCard.setGeometry(QtCore.QRect(30, 90, 141, 261))
+        self.firstCard.setAutoFillBackground(False)
+        self.firstCard.setObjectName(_fromUtf8("firstCard"))
+        self.firstCard_2 = QtGui.QLabel(self.centralwidget)
+        self.firstCard_2.setGeometry(QtCore.QRect(200, 90, 141, 261))
+        self.firstCard_2.setAutoFillBackground(False)
+        self.firstCard_2.setObjectName(_fromUtf8("firstCard_2"))
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtGui.QStatusBar(MainWindow)
         self.statusbar.setObjectName(_fromUtf8("statusbar"))
@@ -54,6 +56,8 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "B-game", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setText(QtGui.QApplication.translate("MainWindow", "Hej och välkommen till B!", None, QtGui.QApplication.UnicodeUTF8))
         self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "PushButton", None, QtGui.QApplication.UnicodeUTF8))
+        self.firstCard.setText(QtGui.QApplication.translate("MainWindow", "FÖSSTA KOTTET", None, QtGui.QApplication.UnicodeUTF8))
+        self.firstCard_2.setText(QtGui.QApplication.translate("MainWindow", "ANDRA KOTTET", None, QtGui.QApplication.UnicodeUTF8))
         self.menuHej_och_v_lkommen_till_B.setTitle(QtGui.QApplication.translate("MainWindow", "Arkiv", None, QtGui.QApplication.UnicodeUTF8))
         self.actionAvsluta.setText(QtGui.QApplication.translate("MainWindow", "Avsluta", None, QtGui.QApplication.UnicodeUTF8))
 
